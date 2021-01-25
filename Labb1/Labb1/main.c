439bee3206dc913a63409569b6e668f9dfa7c408
LeifTeorin <61394735+LeifTeorin@users.noreply.github.com>
2021-01-25 11:26:58 +0100

    here we go

---
+++ Labb1/Labb1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Labb1.c
+ *
+ * Created: 2021-01-25 11:22:42
+ * Author : Hjalmar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
